7b2183055d3c0535bdd5deab591701721a78ef45
AlexisBeltran <jesus_beltran011@outlook.com>
2021-10-28 11:43:24 -0600

    highligth nav added

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 // *****VARIABLES*****
 const Hamburger = document.querySelector('.hamburger img');
 const Navegation = document.querySelector('.navegation');
+const NavLink = document.querySelectorAll('.navegation a');
+const Section = document.querySelectorAll('section');
+
 document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('scroll', ActiveSection);
     Hamburger.addEventListener('click', MenuHamburguesa);
 });
+
 // ****FUNCIONES*****
 const MenuHamburguesa = (e) => {
     e.preventDefault();
@@ -12,4 +17,21 @@ const MenuHamburguesa = (e) => {
     }else{
         Navegation.classList.remove('active');
     }
+}
+
+const ActiveSection = () => {
+    let Current = '';
+    Section.forEach(section => {
+        const SectionTop = section.offsetTop;
+        if(scrollY >= SectionTop){
+            Current = section.getAttribute('id');
+        }
+    });    
+    NavLink.forEach(nav => {
+        nav.classList.remove('active-nav');
+        if(nav.classList.contains(Current)){
+            nav.classList.add('active-nav');
+        }
+    });
+    console.log(Current);
 }
